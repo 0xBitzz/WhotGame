fd5d156c783647bc465e75c1ebba025c1d16e011
0xBitzz <successdaudu@gmail.com>
2023-03-04 16:01:30 +0100

    made changes

--- src/modules/card.py
+++ src/modules/card.py
@@ -1,16 +1,16 @@
 from itertools import chain
 import random
+
 """
-'Whot' - [20, 20, 20, 20, 20]
-'Circle' - [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14]
-'Triangle' - [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14]
-'Cross' - [1, 2, 3, 5, 7, 10, 11, 13, 14]
-'Square', [1, 2, 3, 5, 7, 10, 11, 13, 14]
-'Star', [1, 2, 3, 4, 5, 7, 8]
-"""
-"""
+Whot - [20, 20, 20, 20, 20]
+Circle - [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14]
+Triangle - [1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14]
+Cross - [1, 2, 3, 5, 7, 10, 11, 13, 14]
+Square, [1, 2, 3, 5, 7, 10, 11, 13, 14]
+Star, [1, 2, 3, 4, 5, 7, 8]
+
 Power Numbers
-20 is a wild card that switches the shape,
+20 is a wild card that switches the shape
 14 means general market (everyone pick 1 card),
 8 means skip the next person,
 5 means for the next person to pick up 3 cards (unless you can defend by playing a 5),
@@ -19,84 +19,85 @@ Power Numbers
 """
 
 
-class Circle:
-    def __init__(self):
-        self.cards = self.__setup_card()
-
-    @staticmethod
-    def __setup_card():
-        return [f"Circle {card_num}" for card_num in range(1, 15) if card_num != 6 and card_num != 9]
+def _circle() -> list[str]:
+    return [f"Circle {card_num}" for card_num in range(1, 15) if card_num != 6 and card_num != 9]
 
 
-class Cross:
-    def __init__(self):
-        self.cards = self.__setup_card()
-
-    @staticmethod
-    def __setup_card():
-        return [f"Cross {card_num}" for card_num in range(1, 15) if card_num != 4 and card_num != 6 and card_num != 8
-                and card_num != 9 and card_num != 12]
+def _cross() -> list[str]:
+    return [f"Cross {card_num}" for card_num in range(1, 15) if card_num != 4
+            and card_num != 6 and card_num != 8 and card_num != 9 and card_num != 12]
 
 
-class Square:
-    def __init__(self):
-        self.cards = self.__setup_card()
-
-    @staticmethod
-    def __setup_card():
-        return [f"Square {card_num}" for card_num in range(1, 15) if card_num != 4 and card_num != 6 and card_num != 8
-                and card_num != 9 and card_num != 12]
+def _square() -> list[str]:
+    return [f"Square {card_num}" for card_num in range(1, 15) if card_num != 4
+            and card_num != 6 and card_num != 8 and card_num != 9 and card_num != 12]
 
 
-class Star:
-    def __init__(self):
-        self.cards = self.__setup_card()
-
-    @staticmethod
-    def __setup_card():
-        return [f"Star {card_num}" for card_num in range(1, 9) if card_num != 6]
+def _star() -> list[str]:
+    return [f"Star {card_num}" for card_num in range(1, 9) if card_num != 6]
 
 
-class Triangle:
-    def __init__(self):
-        self.cards = self.__setup_card()
-
-    @staticmethod
-    def __setup_card():
-        return [f"Triangle {card_num}" for card_num in range(1, 15) if card_num != 6 and card_num != 9]
+def _triangle() -> list[str]:
+    return [f"Triangle {card_num}" for card_num in range(1, 15) if card_num != 6 and card_num != 9]
 
 
-class Whot:
-    def __init__(self):
-        self.cards = self.__setup_card()
-
-    @staticmethod
-    def __setup_card():
-        return ["Whot 20" for _ in range(5)]
+def _whot() -> list[str]:
+    return ["Whot 20" for _ in range(5)]
 
 
 class Card:
-    __cards = list(
-        chain.from_iterable(
-            [
-                Circle().cards, Cross().cards, Square().cards,
-                Star().cards, Triangle().cards, Whot().cards
-            ]
-        )
+    _cards_list = list(
+        chain.from_iterable([_circle(), _cross(), _square(), _star(), _triangle(), _whot()])
     )
 
-    def cards(self):
-        return self.__cards
+    _cards = _cards_list[:]
 
-    def shuffle_card(self):
-        return random.shuffle(self.__cards)
+    @property
+    def cards(self) -> list[str]:
+        return self._cards
 
-    def share_card(self):
-        return self.cards().pop()
+    @cards.setter
+    def cards(self, cards) -> None:
+        self._cards = cards
+
+    def shuffle_cards(self) -> None:
+        random.shuffle(self._cards)
 
     @staticmethod
-    def is_power_card(card):
-        card = card.split()[1]
-        if (card == "1") or (card == "2") or (card == "5") or (card == "8") or (card == "14") or (card == "20"):
-            return True
-        return False
+    def is_hold_on(card) -> bool:
+        return Card.is_valid_card(card) and card.split()[-1] == "1"
+
+    @staticmethod
+    def is_pick_two(card) -> bool:
+        return Card.is_valid_card(card) and card.split()[-1] == "2"
+
+    @staticmethod
+    def is_pick_three(card) -> bool:
+        return Card.is_valid_card(card) and card.split()[-1] == "5"
+
+    @staticmethod
+    def is_suspension(card) -> bool:
+        return Card.is_valid_card(card) and card.split()[-1] == "8"
+
+    @staticmethod
+    def is_general_market(card) -> bool:
+        return Card.is_valid_card(card) and card.split()[-1] == "14"
+
+    @staticmethod
+    def is_wild_card(card) -> bool:
+        return Card.is_valid_card(card) and card.split()[-1] == "20"
+
+    @staticmethod
+    def is_power_card(card) -> bool:
+        return Card.is_hold_on(card) or Card.is_pick_two(card) or Card.is_pick_three(card) or \
+            Card.is_suspension(card) or Card.is_general_market(card) or Card.is_wild_card(card)
+
+    @staticmethod
+    def is_valid_card_name(card_name) -> bool:
+        return card_name in [_circle.__name__[1:].title(), _cross.__name__[1:].title(),
+                             _square.__name__[1:].title(), _star.__name__[1:].title(),
+                             _triangle.__name__[1:].title(), _whot.__name__[1:].title()]
+
+    @staticmethod
+    def is_valid_card(card) -> bool:
+        return card in Card._cards_list
--- src/modules/players.py
+++ src/modules/players.py
@@ -2,16 +2,29 @@ from src.modules.card import Card
 
 
 class Player:
-    def __init__(self, player_name):
+    """
+    pick_card(self, cards, num_of_card) specifies number of cards to be picked up by a player
+    place_card(self, card) defines a player playing a card from their stack,
+                            returns the card played popped from their stack
+    """
+    def __init__(self, player_name) -> None:
         self.name = player_name
         self.card_stack: list[Card] = []
-        self.num_of_wins = 0
+        self.win = False
         self.bet_amount = 0
 
-    def pick_card(self, cards, num_of_card):
+    def pick_card(self, cards, num_of_card) -> None:
         for i in range(num_of_card):
             self.card_stack.append(cards.pop())
 
-    def place_card(self, card):
+    def place_card(self, card) -> Card:
         card_index = self.card_stack.index(card)
         return self.card_stack.pop(card_index)
+
+    @staticmethod
+    def has_pick_two(cards) -> bool:
+        return any(Card.is_pick_two(card) for card in cards)
+
+    @staticmethod
+    def has_pick_three(cards) -> bool:
+        return any(Card.is_pick_three(card) for card in cards)
